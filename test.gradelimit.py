1ba2079b696686f4c40b853504d3a945d45fac5e
waduhekx <39086614+waduhekx@users.noreply.github.com>
2018-05-11 17:28:36 +0800

    Update test.gradelimit.py
    能爬到各个大学在湖南的录取分数线（2017-2006），输出文本未正则化

--- test.gradelimit.py
+++ test.gradelimit.py
@@ -1,26 +1,55 @@
 from bs4 import BeautifulSoup
-
 from urllib.request import urlopen
 
-html = urlopen("http://www.hneeb.cn/2018/website/newsDoc/zlzx/2012223111521.html"
-               ).read().decode('GBK')
-
-soup = BeautifulSoup(html,features='lxml')
-
-##print (soup)
-
-all_href= soup.find_all('a')
-t_html=[]
 
 
 
-for i in all_href:
-    t_html.append(i['href'])
+https=[
+'http://www.hneeb.cn/website/newsDoc/zkdt/2016717134751.htm',
+'http://www.hneeb.cn/website/newsDoc/zkdt/2015714225749.htm',
+'http://www.hneeb.cn/website/newsDoc/jyrd/2014714234713.htm',
+'http://www.hneeb.cn/website/newsDoc/jyrd/201371605325.htm',
+'http://www.hneeb.cn/website/newsDoc/jyrd/201271612343.htm',
+'http://www.hneeb.cn/website/newsDoc/jyrd/201171304727.htm',
+'http://www.hneeb.cn/website/newsDoc/zkdt/201071315633.html',
+'http://www.hneeb.cn/website/newsDoc/zkdt/20097131541.html',
+'http://www.hneeb.cn/website/newsDoc/zkdt/200871311951.html',
+'http://www.hneeb.cn/website/newsDoc/zkdt/200771332431.html',
+'http://www.hneeb.cn/website/newsDoc/zkdt/200671355723.html']
 
-for i in t_html:
-    html = urlopen(i)
-    soup = BeautifulSoup(html,features='lxml')
+name_txts=['2016.txt','2015.txt','2014.txt','2013.txt','2012.txt',
+          '2011.txt','2010.txt','2009.txt','2008.txt','2007.txt','2006.txt']
+
+
+http2017='http://www.hneeb.cn/website/newsDoc/jyrd/2017718165024.htm'
+name_txt2017='2017.txt'
+f=open(name_txt2017,'w+')
+try:
+    html=urlopen(http2017)
+    soup=BeautifulSoup(html,features='lxml')
     if soup.find('tr'):
         table = soup.find_all('tr')
-    for j in table:
-        print (j.get_text())
+        for k in table:
+            f.write(k.get_text())
+except Exception as e:
+    print("error!"+j)
+    print(e)
+finally:
+    f.close()
+    
+
+for i,j in zip(https,name_txts):
+    f=open(j,'w+')
+    try:
+        html=urlopen(i)
+        soup=BeautifulSoup(html,features='xml')
+        if soup.find('tr'):
+            table = soup.find_all('tr')
+            for k in table:
+                f.write(k.get_text())
+    except Exception as e:
+        print("error!"+j)
+        print(e)
+        f.close()
+        continue
+    f.close()
