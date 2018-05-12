9a2afef9b4faef8f2c3f0ace10266adc6ab7ab45
luojike <wuqiang2000@foxmail.com>
2018-05-12 22:17:18 +0800

    Add new file for python 2.x

--- test.gradelimit.python2.py
+++ test.gradelimit.python2.py
@@ -1,10 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import re   # for detecting charset in html
+import requests
 from bs4 import BeautifulSoup
-from urllib.request import urlopen
+#from urllib.request import urlopen
 
 
+def getCharset(html):
+    pat = re.compile(r'charset\s*=([a-zA-Z0-9-_]*)[ \t\n\r\f\v">]', re.M | re.I)
+
+    m = pat.search( html, re.M | re.I )
+
+    if m == None :
+        return ""
+    else:
+        return m.group(1)
 
 
-https=[
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+urls=[
 'http://www.hneeb.cn/website/newsDoc/zkdt/2016717134751.htm',
 'http://www.hneeb.cn/website/newsDoc/zkdt/2015714225749.htm',
 'http://www.hneeb.cn/website/newsDoc/jyrd/2014714234713.htm',
@@ -17,34 +35,24 @@ https=[
 'http://www.hneeb.cn/website/newsDoc/zkdt/200771332431.html',
 'http://www.hneeb.cn/website/newsDoc/zkdt/200671355723.html']
 
-name_txts=['2016.txt','2015.txt','2014.txt','2013.txt','2012.txt',
+filenames=['2016.txt','2015.txt','2014.txt','2013.txt','2012.txt',
           '2011.txt','2010.txt','2009.txt','2008.txt','2007.txt','2006.txt']
 
 
-http2017='http://www.hneeb.cn/website/newsDoc/jyrd/2017718165024.htm'
-name_txt2017='2017.txt'
-f=open(name_txt2017,'w+')
-try:
-    html=urlopen(http2017)
-    soup=BeautifulSoup(html,features='lxml')
-    if soup.find('tr'):
-        table = soup.find_all('tr')
-        for k in table:
-            f.write(k.get_text())
-except Exception as e:
-    print("error!"+j)
-    print(e)
-finally:
-    f.close()
-    
-
-for i,j in zip(https,name_txts):
+for i,j in zip(urls, filenames):
     f=open(j,'w+')
     try:
-        html=urlopen(i)
-        soup=BeautifulSoup(html,features='xml')
+        print("\nFetching web page at: " + i + "\n")
+        #html=urlopen(i)
+        r = requests.get(i)
+        charset = getCharset(r.content)
+        print( "\nWeb page encoding is: " + charset + "\n" )
+        r.encoding = charset
+        soup=BeautifulSoup(r.content, features = 'lxml') # after using r.content instead of r.text, encoding of txt file is correct
+                                                        # however, I don't know why
         if soup.find('tr'):
             table = soup.find_all('tr')
+            print("\nWriting to file: " + j + "\n")
             for k in table:
                 f.write(k.get_text())
     except Exception as e:
